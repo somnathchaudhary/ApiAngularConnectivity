0843d9f65c906fa9ced133ba612834df7715c535
somnathchaudhary <somuchaudhary0506@gmail.com>
2020-04-09 19:17:29 +0530

    Api

---
+++ EmployeesApi/EmployeesApi/Controllers/EmpsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeesApi.Models;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeesApi.Controllers
+{
+
+    [EnableCors("CorsPolicy")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpsController : ControllerBase
+    {
+         EmployeesModel[] Employee = new EmployeesModel[]
+        {
+            new EmployeesModel { EmpId = 101, EmpName = "Rahul", EmpDesig = "Doctor"},
+            new EmployeesModel { EmpId = 102, EmpName = "Akahul", EmpDesig = "Doctor"},
+            new EmployeesModel { EmpId = 103, EmpName = "Jahul", EmpDesig = "Doctor"}
+        };
+
+        // GET: api/Emps
+        [HttpGet]
+        public IEnumerable<EmployeesModel> Get()
+        {
+            return Employee;
+        }
+        
+      
+    }
+}
---
+++ EmployeesApi/EmployeesApi/Models/EmployeesModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeesApi.Models
+{
+    public class EmployeesModel
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public string EmpDesig { get; set; }
+
+        
+    }
+}
